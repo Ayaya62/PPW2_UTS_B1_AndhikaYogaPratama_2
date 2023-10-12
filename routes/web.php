69ecbccdd1fc70c024a20093542c69322ac80340
Ayaya62 <andhikayogapratama@mail.ugm.ac.id>
2023-10-12 13:34:11 +0700

    commit

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::resource('products', Controller::class);
+Route::resource('/products', ProductController::class);
+Route::get('/', [ProductController::class, 'index']);
+Route::get('/create', [ProductController::class, 'create']);
+Route::post('/store', [ProductController::class, 'store']);
+
+Route::post('/destroy', [ProductController::class, 'destroy']);
+
+Route::get('/show/{id}', [ProductController::class, 'show']);
+Route::get('/edit/{id}', [ProductController::class, 'edit']);
+Route::post('/update/{id}', [ProductController::class, 'update']);
